9cc16f8d5cd9b389f624f87fd18e698ad0871458
Coding Miku <102744661+toper664@users.noreply.github.com>
2023-11-21 00:12:56 +0700

    feat: changed metadata on site layout

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,17 @@ const montserrat = Montserrat({
 export default async function Home() {
   return (
     <Provider>
-      <AppBar > <LoginLogout></LoginLogout></AppBar> 
-      Hello World
+      <AppBar><LoginLogout></LoginLogout></AppBar> 
+      <br/>
+      <br/>
+      <h1> Apa itu Urang Learn to Drive? </h1>
+      <br/>
+      <p> Urang Learn to Drive adalah perusahaan kursus mengemudi asal Kota Bandung yang didirikan pada tahun 2022. Urang Learn to Drive menjadi pionir dalam lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam purus neque, consequat nec condimentum ac, sagittis ut tortor. Nam malesuada luctus neque vitae euismod. In at scelerisque risus, in sodales mi. Pellentesque ultrices maximus consequat.
+          Nunc vel finibus magna. Nulla vel convallis dui. Duis molestie nunc eu quam ultrices, vel laoreet lorem elementum. Quisque commodo, felis a ultricies lacinia, lectus tortor placerat purus, non scelerisque arcu magna sit amet purus. Nunc pharetra dolor vel pellentesque vestibulum. Nunc a diam feugiat, tincidunt velit quis, mollis libero. Aliquam dignissim et augue quis consectetur.
+          Donec facilisis congue arcu sollicitudin ultricies. Sed sollicitudin vitae urna eget sagittis. Praesent pretium quam et sapien vulputate, sit amet posuere purus vulputate. Maecenas nulla odio, eleifend vitae fermentum in, maximus eget lectus. Donec dapibus elit et tortor ullamcorper cursus. Proin vel luctus lorem. Morbi vehicula massa eget porttitor tristique. Nulla commodo aliquam ullamcorper.
+          Maecenas accumsan turpis ac hendrerit euismod. Curabitur dapibus felis vitae odio congue aliquam. Morbi aliquam tellus et egestas ullamcorper. Pellentesque condimentum sapien odio, eu hendrerit odio lacinia in. Proin scelerisque lectus id aliquet blandit. Aliquam eleifend neque non nulla iaculis, a vestibulum enim sollicitudin. Integer id dolor aliquet mi posuere rhoncus.
+          Morbi auctor arcu vitae tellus placerat, nec ultrices sem dignissim. Vivamus nec turpis neque. Aliquam ut maximus justo. Praesent vehicula faucibus efficitur. Quisque a sem blandit, semper turpis vitae, porta eros. Duis at cursus orci. Nullam dignissim justo metus, ac euismod nunc convallis et. Vivamus vitae nisi elit. Quisque rutrum egestas felis condimentum imperdiet.
+          Duis condimentum erat vitae massa fermentum faucibus.</p>
     </Provider>
 
   )
