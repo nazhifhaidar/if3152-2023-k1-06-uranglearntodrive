16ae7228bd696052f8943f991b87372af41489ea
Hugo Benedicto <18221131@std.stei.itb.ac.id>
2023-11-20 11:12:59 +0000

    feat: add layout admin

---
+++ app/admin/layout.tsx
@@ -0,0 +1,17 @@
+import { getServerSession } from 'next-auth'
+import React from 'react'
+import { options } from '../api/auth/[...nextauth]/options'
+import { redirect } from 'next/navigation';
+
+export default async function adminLayout({children,}: {children: React.ReactNode})
+{
+    const session = await getServerSession(options);
+    if (!session) redirect('/api/auth/signin?callbackUrl=/admin');
+    if (session?.user.role !== "ADMIN"){
+        redirect("/hello");
+    }
+    return (
+        <>{children}</>
+        
+    )
+}
