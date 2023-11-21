c0d106221a4067a8aaea68bc731ace94dd87f736
I Dewa Made Manu P <18221047@std.stei.itb.ac.id>
2023-11-21 18:12:08 +0700

    feat: fix dropdown component

--- app/components/Dropdown/Dropdown.tsx
+++ app/components/Dropdown/Dropdown.tsx
@@ -41,16 +41,17 @@ const Dropdown2: React.FC<DropdownProps> = ({apiLink,label, name}) => {
   return (
     <div style={{paddingBottom:'4px'}}>
       <label className='pb[8px]' style={{display: 'flex'}}>{label}</label>
-      <select name={name} value={selectedValue} onChange={handleSelect} disabled={loading} style={{paddingBottom:'4px', width:'450px', paddingLeft: '4px', border: '2px solid #ccc'}}>
-        <option value="" disabled>
-          {loading ? 'Loading options...' : 'Select an option'}
+      <select name={name} value={selectedValue} onChange={handleSelect} disabled={options===null} style={{paddingBottom:'4px', width:'450px', paddingLeft: '4px', border: options!== null?'2px solid #ccc': '2px solid red'}}>
+        <option value="" disabled >
+          {loading ? 'Loading options...' : options!== null? 'Select an option':'Data tidak tersedia'}
         </option>
-        {!loading && 
-          options.map((option) => (
+        {(options !== null) ?
+          (options.map((option) => (
             <option key={option.id} value={option.id}>
               {option.id}
-            </option>
-          ))}
+            </option>)
+            )) : 
+          <option value="" disabled></option>}
       </select>
     </div>  
   );
