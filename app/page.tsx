870993f17d0aa44451bc6520fbb5ed5d347ff6ef
Coding Miku <102744661+toper664@users.noreply.github.com>
2023-11-23 11:15:50 +0700

    feat: updated dashboard content with comp and img

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import AppBar from '@/app/components/AppBar';
 import LoginLogout from '@/app/utils/loginlogout';
 import { Montserrat } from 'next/font/google';
 import Row from '@/app/components/Row';
+import Button2 from './components/Buttons/Button2';
+import SectionContainer from './components/Containers/SectionContainer';
 
 const montserrat = Montserrat({
   weight: '400',
@@ -36,14 +39,38 @@ const HomePage: React.FC = async () => {
       </AppBar>
       <br/>
       <br/>
-      <h1> Apa itu Urang Learn to Drive? </h1>
+      <div style={{display:'flex', flexDirection:'row'}}>
+        <h1 style={{paddingLeft:'4rem', fontSize:'72px', display:'flex', flexDirection:'row', maxWidth:'60%', fontStyle:'italic'}}> Mengemudi bersama Urang Learn to Drive </h1>
+        <Image src='/D.jpg' width={720} height={540} alt="resing" style={{marginTop:'1rem', paddingRight:'4rem'}}></Image>
+      </div>
+      <div style={{display:'flex', marginTop:'2rem', flexDirection:'row'}}>
+        <div style={{paddingLeft:'4rem', maxWidth:'35%'}}>
+          <p> Urang Learn to Drive menyediakan layanan kelas kursus mengemudi mobil manual maupun matic. Terdapat lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam purus neque, consequat nec condimentum ac, sagittis ut tortor. Nam malesuada luctus neque vitae euismod. In at scelerisque risus, in sodales mi. Pellentesque ultrices maximus consequat.
+              Nunc vel finibus magna. Nulla vel convallis dui. Duis molestie nunc eu quam ultrices, vel laoreet lorem elementum. Quisque commodo, felis a ultricies lacinia, lectus tortor placerat purus, non scelerisque arcu magna sit amet purus.</p>
+          <Link href='/classlist' style={{marginLeft:'-8px'}}>
+            <Button2 text='Cari Kelas' type='button'></Button2>
+          </Link>
+        </div>
+        <div style={{marginLeft:'2rem', marginRight:'4rem'}}>
+          <SectionContainer w={100} bg_col='azure' bdr_col='' children={
+            <>
+              <div style={{display:'flex', flexDirection:'column'}}>
+                <h1> Panduan sebelum mulai mendaftar... </h1>
+                <p style={{textAlign:'left'}}> 1. Pastikan bahwa lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam purus neque, consequat nec condimentum ac, sagittis ut tortor. Nam malesuada luctus neque vitae euismod. In at scelerisque risus, in sodales mi. Pellentesque ultrices maximus consequat.
+                Nunc vel finibus magna. Nulla vel convallis dui. Duis molestie nunc eu quam ultrices, vel laoreet lorem elementum. Quisque commodo, felis a ultricies lacinia, lectus tortor placerat purus, non scelerisque arcu magna sit amet purus.</p>
+                <p style={{textAlign:'left'}}> 2. Siapkan terlebih dahulu lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam purus neque, consequat nec condimentum ac, sagittis ut tortor. Nam malesuada luctus neque vitae euismod. In at scelerisque risus, in sodales mi. Pellentesque ultrices maximus consequat.
+                Nunc vel finibus magna. Nulla vel convallis dui. Duis molestie nunc eu quam ultrices, vel laoreet lorem elementum. Quisque commodo, felis a ultricies lacinia, lectus tortor placerat purus, non scelerisque arcu magna sit amet purus.</p>
+                <p style={{textAlign:'left'}}> 3. Rata-rata waktu yang diperlukan adalah lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam purus neque, consequat nec condimentum ac, sagittis ut tortor. Nam malesuada luctus neque vitae euismod. In at scelerisque risus, in sodales mi. Pellentesque ultrices maximus consequat.
+                Nunc vel finibus magna. Nulla vel convallis dui. Duis molestie nunc eu quam ultrices, vel laoreet lorem elementum. Quisque commodo, felis a ultricies lacinia, lectus tortor placerat purus, non scelerisque arcu magna sit amet purus.</p>
+                <p style={{textAlign:'left'}}> 4. Setelah selesai, anda akan lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam purus neque, consequat nec condimentum ac, sagittis ut tortor. Nam malesuada luctus neque vitae euismod. In at scelerisque risus, in sodales mi. Pellentesque ultrices maximus consequat.
+                Nunc vel finibus magna. Nulla vel convallis dui. Duis molestie nunc eu quam ultrices, vel laoreet lorem elementum. Quisque commodo, felis a ultricies lacinia, lectus tortor placerat purus, non scelerisque arcu magna sit amet purus.</p>
+              </div>
+            </>
+          }></SectionContainer>
+        </div>
+      </div>
       <br/>
-      <p style={{paddingLeft:'4rem', paddingRight:'4rem'}}> Urang Learn to Drive adalah perusahaan kursus mengemudi asal Kota Bandung yang didirikan pada tahun 2022. Urang Learn to Drive menjadi pionir dalam lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam purus neque, consequat nec condimentum ac, sagittis ut tortor. Nam malesuada luctus neque vitae euismod. In at scelerisque risus, in sodales mi. Pellentesque ultrices maximus consequat.
-          Nunc vel finibus magna. Nulla vel convallis dui. Duis molestie nunc eu quam ultrices, vel laoreet lorem elementum. Quisque commodo, felis a ultricies lacinia, lectus tortor placerat purus, non scelerisque arcu magna sit amet purus. Nunc pharetra dolor vel pellentesque vestibulum. Nunc a diam feugiat, tincidunt velit quis, mollis libero. Aliquam dignissim et augue quis consectetur.
-          Donec facilisis congue arcu sollicitudin ultricies. Sed sollicitudin vitae urna eget sagittis. Praesent pretium quam et sapien vulputate, sit amet posuere purus vulputate. Maecenas nulla odio, eleifend vitae fermentum in, maximus eget lectus. Donec dapibus elit et tortor ullamcorper cursus. Proin vel luctus lorem. Morbi vehicula massa eget porttitor tristique. Nulla commodo aliquam ullamcorper.
-          Maecenas accumsan turpis ac hendrerit euismod. Curabitur dapibus felis vitae odio congue aliquam. Morbi aliquam tellus et egestas ullamcorper. Pellentesque condimentum sapien odio, eu hendrerit odio lacinia in. Proin scelerisque lectus id aliquet blandit. Aliquam eleifend neque non nulla iaculis, a vestibulum enim sollicitudin. Integer id dolor aliquet mi posuere rhoncus.
-          Morbi auctor arcu vitae tellus placerat, nec ultrices sem dignissim. Vivamus nec turpis neque. Aliquam ut maximus justo. Praesent vehicula faucibus efficitur. Quisque a sem blandit, semper turpis vitae, porta eros. Duis at cursus orci. Nullam dignissim justo metus, ac euismod nunc convallis et. Vivamus vitae nisi elit. Quisque rutrum egestas felis condimentum imperdiet.
-          Duis condimentum erat vitae massa fermentum faucibus.</p>
+      
     </>
   )
 }
