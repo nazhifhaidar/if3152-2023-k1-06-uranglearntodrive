fccb84093d887d59de58310496bd46c03a845316
I Dewa Made Manu P <18221047@std.stei.itb.ac.id>
2023-11-25 16:07:01 +0700

    feat: fix error on create admin page

--- app/owner/manage-admin/create/page.tsx
+++ app/owner/manage-admin/create/page.tsx
@@ -10,18 +10,9 @@ import {useContext} from 'react';
 import Toast from '@/app/components/Toast/Toast'
 import { useSession } from 'next-auth/react'
 import { redirect } from 'next/navigation'
+import { getServerSession } from 'next-auth'
 
 const CreatePage:React.FC = () => {
-    const { data: session} = useSession({
-        onUnauthenticated() {
-            redirect('/api/auth/signin?callbackUrl=/check');
-        },
-        required:true
-    });
-    if (!session) redirect('/api/auth/signin?callbackUrl=/check');
-    if (session?.user.role !== "OWNER"){
-        redirect("/");
-    }
   return (
     <div>
 <AppBar > <LoginLogout></LoginLogout></AppBar> 
